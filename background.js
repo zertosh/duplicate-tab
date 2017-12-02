4bc96abb15a74e344fd5a8ed5a9167a73ed19e6d
Andres Suarez <zertosh@gmail.com>
2017-12-02 15:33:29 -0500

    Use arrow functions

--- background.js
+++ background.js
@@ -1,13 +1,13 @@
 'use strict';
 /* global chrome */
 
-chrome.browserAction.onClicked.addListener(function(tab) {
+chrome.browserAction.onClicked.addListener(tab => {
   chrome.tabs.duplicate(tab.id);
 });
 
-chrome.commands.onCommand.addListener(function(command) {
+chrome.commands.onCommand.addListener(command => {
   if (command === 'duplicate-tab-keyboard-shortcut') {
-    chrome.tabs.getSelected(null, function(tab) {
+    chrome.tabs.getSelected(null, tab => {
       chrome.tabs.duplicate(tab.id);
     });
   }
