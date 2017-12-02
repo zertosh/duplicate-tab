8b4c6c3dae56633652097a127cd43e4404ab9767
Andres Suarez <zertosh@gmail.com>
2017-12-02 15:43:12 -0500

    Add "close-other-tabs-shortcut" command

--- background.js
+++ background.js
@@ -10,5 +10,13 @@ chrome.commands.onCommand.addListener(command => {
     chrome.tabs.getSelected(null, tab => {
       chrome.tabs.duplicate(tab.id);
     });
+  } if (command === 'close-other-tabs-shortcut') {
+    chrome.tabs.query({
+      active: false,
+      pinned: false,
+      currentWindow: true,
+    }, tabList => {
+      chrome.tabs.remove(tabList.map(tab => tab.id));
+    });
   }
 });
